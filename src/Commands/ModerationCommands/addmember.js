e4df431894dfb177a03c501ac371725b332a336a
Ruben <adityaprasad7123@gmail.com>
2023-10-04 01:22:52 -0400

    Made addmember command

---
+++ src/Commands/ModerationCommands/addmember.js
@@ -0,0 +1,32 @@
+const addMemberCommand = async (client, message, args) => {
+    const group = await message.getChat();
+
+    if (!group.isGroup) {
+        return message.reply("You can only add members to a group chat!");
+    }
+
+    const contactIDs = args.slice(1).map(item => item + "@c.us");
+    const participants = group.participants;
+
+    const membersToAdd = [];
+
+    for (const contactId of contactIDs) {
+        const isAlreadyInGroup = participants.some(participant => participant.id._serialized === contactId);
+
+        if (isAlreadyInGroup) {
+            client.sendMessage(message.from, `${contactId} is already in the group chat.`);
+        } else {
+            membersToAdd.push(contactId);
+        }
+    }
+
+    if (membersToAdd.length === 0) {
+        return client.sendMessage(message.from, "No Members to Add!");
+    }
+
+    await group.addParticipants(membersToAdd);
+
+    client.sendMessage(message.from, "Added Members ✅");
+};
+
+module.exports = addMemberCommand;
